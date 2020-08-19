1b722b8e52fd41e416c0cf7361bc72c78c3dd26c
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2020-08-19 17:17:45 -0300

    comit.

--- exemplo.py
+++ exemplo.py
@@ -34,4 +34,4 @@ print(numeroDecimal)
 print(round(numeroDecimal,3))
 #Outra forma de arredondamento e definir a quantidade de casas depois da vírgula(pontos)
 print('%.3f' % numeroDecimal)
-#Ver para truncar casas(próxima aula)
+#Ver para truncar casas(próxima aula) adasdasdasda
